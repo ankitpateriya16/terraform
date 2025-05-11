021aa95ecce7a16335d9ceeaf04c4b692108f380
ankitpateriya16 <ankitpateriya161008@gmail.com>
2025-05-11 15:21:11 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "public_ip_1" {
   description = "Public IP address of the first instance"
-  value       = aws_instance.instance_1.public_ip
+  value       = aws_instance.example-0.public_ip
 }
 
 output "public_ip_2" {
   description = "Public IP address of the second instance"
-  value       = aws_instance.instance_2.public_ip
+  value       = aws_instance.example-1.public_ip
 }
 output "private_key_pem" {
   value     = tls_private_key.example.private_key_pem
