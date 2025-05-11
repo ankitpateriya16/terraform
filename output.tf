6096ea242e185c4a47c5b54194e823909761e9bf
ankitpateriya16 <ankitpateriya161008@gmail.com>
2025-05-11 15:19:22 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-output "public_ips" {
-  description = "Public IPs of all EC2 instances"
-  value       = [for instance in aws_instance.example : instance.public_ip]
+output "public_ip_1" {
+  description = "Public IP address of the first instance"
+  value       = aws_instance.instance_1.public_ip
 }
- 
-output "private_ips" {
-  description = "Private IPs of all EC2 instances"
-  value       = [for instance in aws_instance.example : instance.private_ip]
+
+output "public_ip_2" {
+  description = "Public IP address of the second instance"
+  value       = aws_instance.instance_2.public_ip
 }
 output "private_key_pem" {
   value     = tls_private_key.example.private_key_pem
