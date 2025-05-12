92f7949d608c71ee2a99ca228215e34135044532
ankitpateriya16 <ankitpateriya161008@gmail.com>
2025-05-12 16:23:17 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,10 @@
 variable "ami" {
-  default = "ami-058a8a5ab36292159"
+  default = "ami-084568db4383264d4"
  type = string
 }
 variable "instance_type" {
  default = "t2.micro"
  type = string
 }
+#ami-084568db4383264d4 us-east-1
+#ami-058a8a5ab36292159 us-east-2
