5ed6782b217b02dccf25b46189e65ca300f077f2
ankit pateriya <ankitpateriya161008@gmail.com>
2025-05-14 13:54:25 +0000

    adding files

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  region = "us-east-1"
 }
 resource "tls_private_key" "example" {
   algorithm = "RSA"
@@ -15,19 +16,36 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-vpc_id = data.aws_vpc.default.id
- 
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Allow SSH, HTTP, Jenkins"
+  vpc_id = data.aws_vpc.default.id
   ingress {
+    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
  
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  ingress {
+    description = "Allow Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -35,12 +53,12 @@ vpc_id = data.aws_vpc.default.id
   }
 }
 resource "aws_instance" "example" {
-  count         = 2
+  count         = 3
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = aws_key_pair.generated_key.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
   tags = {
-    Name = "example-${count.index}"
+    Name = var.instance_name[count.index]
   }
-}
+} 
--- variable.tf
+++ variable.tf
@@ -6,5 +6,9 @@ variable "instance_type" {
  default = "t2.micro"
  type = string
 }
+variable "instance_name" {
+ default = ["jenkins-server", "ansible_server" , "Docker-host"]
+}
+
 #ami-084568db4383264d4 us-east-1
 #ami-058a8a5ab36292159 us-east-2
